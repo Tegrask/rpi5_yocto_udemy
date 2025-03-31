c0041396b126633c77f239d745a38a97a594835c
Tegrask <ngoumkouearold2@gmail.com>
2025-03-31 19:04:51 +0200

    delete a buils folders from recipes-app

--- sources/meta-moz/recipes-apps/yoctoqt6demo/files/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/appyoctoqt6demo_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appyoctoqt6demo")
-set(working_dir "C:/Users/Hytega/OneDrive/Desktop/yoctoqt6demo")
-set(src_and_dest_list
-    "C:/Users/Hytega/OneDrive/Desktop/yoctoqt6demo/Main.qml"
-    "C:/Users/Hytega/OneDrive/Desktop/yoctoqt6demo/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/yoctoqt6demo/Main.qml"
-
-)
-set(timestamp_file "C:/Users/Hytega/OneDrive/Desktop/yoctoqt6demo/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/appyoctoqt6demo_qml.txt")
